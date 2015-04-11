870684a9713d03b05516e707d1bee9caf2b382d0
metal-crow <frankcrisanti@aol.com>
2015-04-11 00:16:39 -0400

    work in progress on usb thread

--- src/USB_Thread.java
+++ src/USB_Thread.java
@@ -1,4 +1,8 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.RandomAccessFile;
@@ -14,6 +18,7 @@ public class USB_Thread extends Thread {
     private boolean useiTunesDataLibraryFile;
     private RandomAccessFile readituneslibrary;
     private String iTunesDataLibraryFile;
+    private boolean listen=true;
     
     public USB_Thread(String adbExe, String musicDirectoryPath, String convertMusicTo, boolean useiTunesDataLibraryFile, RandomAccessFile readituneslibrary, String iTunesDataLibraryFile) {
         this.adbExe=adbExe;
@@ -41,7 +46,7 @@ public class USB_Thread extends Thread {
     @Override
     public void run() {
         //loop and listen for connection
-        while(Desktop_Server.listen){
+        while(listen){
             //check to see if a phone is plugged in
             boolean newConnection=false;
             try{
@@ -62,15 +67,104 @@ public class USB_Thread extends Thread {
                 e.printStackTrace();
             }
             
+            //sync
             if(newConnection && !connection){
                 connection=true;//phone is connected, dont resync
                 System.out.println("Sync Connection via USB");
                 
-                //generate the list of all songs
-                ArrayList<String> songs=new ArrayList<String>();
-                Desktop_Server.generateList(songs, musicDirectoryPath);
-
+                ArrayList<String> listOfSongsToAdd=new ArrayList<String>();
                 
+                //get list of songs the phone has
+                //first copy file from phone to local
+                Runtime runtime = Runtime.getRuntime();
+                Process p;
+                try {
+                    p = runtime.exec(adbExe+" pull /extSdCard/SongSync/SongSync_Song_List.txt /");
+                    p.waitFor();
+                    //read this into memory
+                    BufferedReader in = new BufferedReader(new FileReader("SongSync_Song_List.txt"));
+                    ArrayList<String> master_song_list=new ArrayList<String>();
+                    while(in.ready()){
+                        master_song_list.add(in.readLine());
+                    }
+                    
+                    //generate the list of all songs
+                    ArrayList<String> songs=new ArrayList<String>();
+                    Desktop_Server.generateList(songs, musicDirectoryPath);
+                    
+                    //compare
+                    FileWriter out=new FileWriter("SongSync_Song_List.txt.new");
+                    for(String recieve:songs){
+                        //i dont know why android uses a / for a file delimiter and windows uses a \.
+                        recieve=recieve.replaceAll("\\\\", "/");
+                        
+                        //for each song title, check if we already have it on the phone
+                        //and we are doing a normal sync
+                        if(master_song_list.contains(recieve) && Desktop_Server.sync_type=="N"){
+                            //if so, we remove it from the list. At the end, the songs remaining in the list no longer exist on the pc and will be removed from the phone
+                            master_song_list.remove(recieve);
+                            //also write this song, which we have, to the master list
+                            out.write(recieve+"\n");
+                        }
+                        else{
+                            //if it isnt in the previous master list, or we are doing a full resync, we need to get it
+                            listOfSongsToAdd.add(recieve);
+                        }
+                    }
+                    in.close();
+                    out.close();
+                    new File("SongSync_Song_List.txt.new").renameTo(new File("SongSync_Song_List.txt"));
+                    out=new FileWriter("SongSync_Song_List.txt");
+                    
+                    //out now has the the same as in, missing songs we remove. do those now
+                    for(String rmSong:master_song_list){
+                        p=runtime.exec(adbExe+" shell rm "+rmSong);
+                        p.waitFor();
+                    }
+                    //push the updated out file, which matches the music to not include the files we just removed
+                    p=runtime.exec(adbExe+" shell rm /extSdCard/SongSync/SongSync_Song_List.txt");
+                    p.waitFor();
+                    p=runtime.exec(adbExe+" push SongSync_Song_List.txt /extSdCard/SongSync");
+                    p.waitFor();
+                    
+                    //for all the new songs
+                    for(String newSong:listOfSongsToAdd){
+                        //convert the song
+                        String songpath=musicDirectoryPath+newSong;
+                        System.out.println("got request for "+newSong);
+                        
+                        //find the songs filetype, and convert it if it needs to be converted
+                        String filetype=songpath.substring(songpath.lastIndexOf("."));
+                        //if we're using iTunes we always need to remux to add iTunes metadata and art
+                        if((!convertMusicTo.equals("") && !filetype.equals(convertMusicTo)) || useiTunesDataLibraryFile){
+                                String metadata="";
+                                if(useiTunesDataLibraryFile){
+                                    metadata=iTunesInterface.scanForitunesMetadata(newSong,readituneslibrary,iTunesDataLibraryFile);
+                                }
+                                try {
+                                    Desktop_Server.conversion(songpath, metadata);
+                                } catch (IOException e) {
+                                    System.err.println(e.getMessage()+". Aborting sync.");
+                                    out.close();
+                                    in.close();
+                                    System.exit(0);
+                                }
+                                //change the file to point to the converted song
+                                songpath="tempout"+convertMusicTo;
+                        }
+
+                        //write song to phone
+                        p=runtime.exec(adbExe+" push tempout"+convertMusicTo+" /extSdCard/SongSync/Music/");
+                        p.waitFor();
+
+                        //append the new written song to the txt list, and push the updated list to the phone. This is slow, but allows resume from interrupts.
+
+                    }
+                    
+                } catch (IOException | InterruptedException e) {
+                    System.err.println("Unrecoverable usb reading error.");
+                    e.printStackTrace();
+                }
             }
         }
         System.out.println("USB listener exiting.");
@@ -84,5 +178,10 @@ public class USB_Thread extends Thread {
             System.err.println("unable to kill usb listener.");
             e.printStackTrace();
         }
+        listen=false;
+    }
+    
+    public void try_force_connection(){
+        connection=true;
     }
 }
